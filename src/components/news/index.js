7a33ce7332f0c84ef47e7b8b961e4c5641c28dd3
collins njau <collinsnjau39@gmail.com>
2019-11-11 16:05:21 +0300

    Add paginator

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -4,9 +4,15 @@ import { getNewsArticles } from "../../redux/actions/newsAction";
 import { searchNewsArticles } from "../../redux/actions/searchNewsActions";
 import { NewsArticle } from "./newsArticles";
 import { SelectBar } from "../selectBar/index";
+import Paginator from "./paginator";
 import "./news.css";
 
 class News extends Component {
+  state = {
+    currentPage: 1,
+    newsPerPage: 4,
+    newsArticles: []
+  };
   componentDidMount() {
     const { getNewsArticles } = this.props;
     getNewsArticles();
@@ -26,8 +32,34 @@ class News extends Component {
     return false;
   }
 
+  /**
+   * handle click of paginator
+   */
+  handlePaginatorClick = event => {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  };
+
+  addToArray = currentNews => {
+    const { news } = this.props;
+    if (news.length >= 1) {
+      const pageNumbers = [];
+      for (let i = 1; i <= Math.ceil(news.length / currentNews); i++) {
+        pageNumbers.push(i);
+      }
+      return pageNumbers;
+    }
+  };
+
   render() {
     const { news, isLoading } = this.props;
+    const { currentPage, newsPerPage } = this.state;
+    // logic for displaying news
+    const indexOfTheLastNews = currentPage * newsPerPage;
+    const indexOfFirstNews = indexOfTheLastNews - newsPerPage;
+    const currentNews = news.slice(indexOfFirstNews, indexOfTheLastNews);
+
     return (
       <div>
         {isLoading ? (
@@ -37,7 +69,12 @@ class News extends Component {
             <SelectBar />
             {this.checkNewsArticles(news) ? (
               <div>
-                <NewsArticle news={news} />
+                <NewsArticle news={currentNews} />
+                <Paginator
+                  currentNews={currentNews}
+                  news={news}
+                  handlePaginatorClick={this.handlePaginatorClick}
+                />
               </div>
             ) : (
               <div>
---
+++ src/components/news/paginator.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Paginator extends Component {
+  computePaginator = (currentNews, news) => {
+    const pageNumbers = [];
+
+    const number = Math.ceil(news.length / currentNews.length);
+    console.log(number);
+    for (let i = 1; i <= number; i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  };
+  render() {
+    const { currentNews, news, handlePaginatorClick } = this.props;
+    const pageNumbers = this.computePaginator(currentNews, news);
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <button
+          key={number}
+          id={number}
+          onClick={handlePaginatorClick}
+          className="paginator_key"
+        >
+          {number}
+        </button>
+      );
+    });
+
+    return <div className="paginator_container">{renderPageNumbers}</div>;
+  }
+}
+
+export default Paginator;
