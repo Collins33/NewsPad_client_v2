1eb7550a54ec97d9e347270487423270cd222e9d
collins njau <collinsnjau39@gmail.com>
2019-11-15 15:33:49 +0300

    Refactor the selectbar

--- src/components/selectBar/index.js
+++ src/components/selectBar/index.js
@@ -1,73 +1,36 @@
 import React, { Component } from "react";
 import "./index.css";
 class SelectBar extends Component {
+  state = {
+    categories: [
+      { value: "business" },
+      { value: "bitcoin" },
+      { value: "politics" },
+      { value: "sports" },
+      { value: "cars" },
+      { value: "technology" },
+      { value: "entertainment" },
+      { value: "education" }
+    ]
+  };
   render() {
     const { handleSelectBarClick } = this.props;
+    const { categories } = this.state;
+    const buttons = categories.map((category, index) => (
+      <button
+        className="select__button"
+        value={category.value}
+        onClick={handleSelectBarClick}
+      >
+        {category.value.toUpperCase()}
+      </button>
+    ));
     return (
       <div className="select_bar">
         <span className="popular_news_categories_text">
           POPULAR NEWS CATEGORIES
         </span>
-        <div className="select_bar_container">
-          <button
-            className="select__button"
-            value="business"
-            onClick={handleSelectBarClick}
-          >
-            Business
-          </button>
-
-          <button
-            className="select__button"
-            value="bitcoin"
-            onClick={handleSelectBarClick}
-          >
-            Bitcoin{" "}
-          </button>
-
-          <button
-            className="select__button"
-            value="politics"
-            onClick={handleSelectBarClick}
-          >
-            Politics
-          </button>
-          <button
-            className="select__button"
-            value="sports"
-            onClick={handleSelectBarClick}
-          >
-            Sports{" "}
-          </button>
-          <button
-            className="select__button"
-            value="cars"
-            onClick={handleSelectBarClick}
-          >
-            Cars
-          </button>
-          <button
-            className="select__button"
-            value="technology"
-            onClick={handleSelectBarClick}
-          >
-            Technology{" "}
-          </button>
-          <button
-            className="select__button"
-            value="entertainment"
-            onClick={handleSelectBarClick}
-          >
-            Entertainment
-          </button>
-          <button
-            className="select__button"
-            value="education"
-            onClick={handleSelectBarClick}
-          >
-            Education{" "}
-          </button>
-        </div>
+        <div className="select_bar_container">{buttons}</div>
       </div>
     );
   }
