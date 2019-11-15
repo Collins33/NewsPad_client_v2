d8851d4a302c3a3a55a624ac8449ccf69af6d65f
collins njau <collinsnjau39@gmail.com>
2019-11-15 14:24:39 +0300

    Make cards resposive

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import { Header } from "./components/header/Header";
 import Search from "./components/search/Search";
 import News from "./components/news/index";
-
 require("dotenv").config();
 
 function App() {
   return (
-    <>
+    <div className="">
       <Header />
       <Search />
       <News />
-    </>
+    </div>
   );
 }
 
