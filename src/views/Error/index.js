9d36617496d9f5be68822098cccbb5b948d54115
collins njau <collinsnjau39@gmail.com>
2019-11-19 15:30:54 +0300

    Style the error component

--- src/views/Error/index.js
+++ src/views/Error/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.css";
 
 const RouteError = () => (
   <div>
-    <button>
+    <h1 className="header__content">
+      Oooops Seems the requested page does not exist
+    </h1>
+    <button className="back_button">
       <Link to="/">Return to homepage</Link>
     </button>
   </div>
