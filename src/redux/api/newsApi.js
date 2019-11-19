03d96700346003e9b66fc66ad82b47f884749292
collins njau <collinsnjau39@gmail.com>
2019-11-19 18:23:10 +0300

    Change api endpoint

--- src/redux/api/newsApi.js
+++ src/redux/api/newsApi.js
@@ -5,7 +5,7 @@ class NewsApi {
   static getNewsArticles() {
     const newsApiKey = process.env.REACT_APP_NEWS_API_KEY;
     return axios.get(
-      `https://newsapi.org/v2/everything?q=all&apiKey=${newsApiKey}`
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${newsApiKey}`
     );
   }
 }
