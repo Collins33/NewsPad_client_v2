0a8a9bf07c164ee46323e1436bff0da8dc9c123a
collins njau <collinsnjau39@gmail.com>
2019-11-19 15:22:49 +0300

    Add error page - add page for missing url

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "../views/Home/index";
 import MyArticles from "../views/MyArticles/index";
+import RouteError from "../views/Error/index";
 import { Route, Switch } from "react-router-dom";
 
 const Routes = () => (
   <Switch>
     <Route path="/" component={Home} exact />
     <Route path="/myarticles" component={MyArticles} exact />
+    <Route path="*" component={RouteError} />
   </Switch>
 );
 
---
+++ src/views/Error/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const RouteError = () => (
+  <div>
+    <button>
+      <Link to="/">Return to homepage</Link>
+    </button>
+  </div>
+);
+
+export default RouteError;
