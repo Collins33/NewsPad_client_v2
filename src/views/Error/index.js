611753d32ae981735ca34d9749963ddb27ecbeb6
collins njau <collinsnjau39@gmail.com>
2019-11-20 17:10:51 +0300

    Refactor the error page

--- src/views/Error/index.js
+++ src/views/Error/index.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import "./index.scss";
 
 const RouteError = () => (
-  <div>
+  <div className="error_page_container">
     <h1 className="header__content">
-      Oooops Seems the requested page does not exist
+      Looks like the page you were looking for is no longer here.
     </h1>
     <button className="back_button">
       <Link to="/">Return to homepage</Link>
