b154ed4da927faf0a28584b9ab05de5531192ff9
collins njau <collinsnjau39@gmail.com>
2019-11-20 15:58:08 +0300

    Fix styling of the button

--- src/components/news/newsArticles.js
+++ src/components/news/newsArticles.js
@@ -4,20 +4,21 @@ import "./news.scss";
 export const NewsArticle = ({ news, checkNewsImage }) => {
   const displayNews = news.map((story, index) => (
     <div key={index} className="news-card">
-      <div>
-        {checkNewsImage(story.urlToImage) ? (
-          <img
-            src="https://images.unsplash.com/photo-1559526324-c1f275fbfa32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-            className="news-image"
-            alt="images"
-          />
-        ) : (
-          <img src={story.urlToImage} className="news-image" alt="images" />
-        )}
-      </div>
+      {checkNewsImage(story.urlToImage) ? (
+        <img
+          src="https://images.unsplash.com/photo-1559526324-c1f275fbfa32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+          className="news-image"
+          alt="images"
+        />
+      ) : (
+        <img src={story.urlToImage} className="news-image" alt="images" />
+      )}
 
       <div>
-        <h1 className="news_author">{story.source.name}</h1>
+        <h4 className="news_author">{story.source.name}</h4>
+      </div>
+      <div>
+        <h1>{story.title}</h1>
       </div>
       <button className="search__button">Read more >>></button>
     </div>
