ebddae7eb46c2181db3a295e000a901fd3911cf7
collins njau <collinsnjau39@gmail.com>
2019-11-28 16:35:54 +0300

    Add service worker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store/configureStore";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+
 require("dotenv").config();
 
 ReactDOM.render(
@@ -15,3 +17,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+serviceWorker.register();
