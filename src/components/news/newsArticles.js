312022581267d5fbd752bd994ccd1ed4f787f443
collins njau <collinsnjau39@gmail.com>
2020-01-20 18:14:16 +0300

    Separate save button from the rest of the news div

--- src/components/news/newsArticles.js
+++ src/components/news/newsArticles.js
@@ -9,30 +9,28 @@ export const NewsArticle = ({
   getSingleNewsArticle
 }) => {
   const displayNews = news.map((story, index) => (
-    <a
-      target="_blank"
-      href={story.url}
-      rel="noopener noreferrer"
-      className="news-card"
-      key={index}
-    >
-      <div key={index} className="card_parent">
-        {checkNewsImage(story.urlToImage) ? (
-          <img
-            src="https://images.unsplash.com/photo-1559526324-c1f275fbfa32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-            className="news-image"
-            alt="news-images"
-          />
-        ) : (
-          <img src={story.urlToImage} className="news-image" alt="images" />
-        )}
+    <div className="news-card">
+      <a target="_blank" href={story.url} rel="noopener noreferrer" key={index}>
+        <div key={index} className="card_parent">
+          {checkNewsImage(story.urlToImage) ? (
+            <img
+              src="https://images.unsplash.com/photo-1559526324-c1f275fbfa32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+              className="news-image"
+              alt="news-images"
+            />
+          ) : (
+            <img src={story.urlToImage} className="news-image" alt="images" />
+          )}
 
-        <div className="content_container">
-          <p className="news_author">{story.source.name}</p>
-          <div className="news_title_container">
-            <p className="news_title">{story.title}</p>
+          <div className="content_container">
+            <p className="news_author">{story.source.name}</p>
+            <div className="news_title_container">
+              <p className="news_title">{story.title}</p>
+            </div>
           </div>
         </div>
+      </a>
+      <div>
         {email ? (
           <div>
             <img
@@ -46,7 +44,7 @@ export const NewsArticle = ({
           ""
         )}
       </div>
-    </a>
+    </div>
   ));
   return <div className="news_article_container">{displayNews}</div>;
 };
