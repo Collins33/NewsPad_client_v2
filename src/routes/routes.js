a874e3beb7f449ad70628bd42487edb95461752d
collins njau <collinsnjau39@gmail.com>
2020-01-21 15:35:13 +0300

    Remove unused code

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Home from "../views/Home/index";
-import Header from "../components/header/Header";
 import MyArticles from "../views/MyArticles/index";
 import RouteError from "../views/Error/index";
 import { Route, Switch } from "react-router-dom";
 
 const Routes = () => (
   <>
-    {/* <Header /> */}
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/myarticles" component={MyArticles} exact />
