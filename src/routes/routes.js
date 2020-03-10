1d6916ae9cbe059c82587fcd0366f2f1e033367a
collins njau <collinsnjau39@gmail.com>
2020-03-10 21:25:08 +0300

    Fix navbar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import Home from "../views/Home/index";
 import MyArticles from "../views/MyArticles/index";
 import RouteError from "../views/Error/index";
+import Header from "../components/header/Header";
 import { Route, Switch } from "react-router-dom";
 
 const Routes = () => (
   <>
+    <Header />
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/profile" component={MyArticles} exact />
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import News from "../../components/news/index";
-import Header from "../../components/header/Header";
 class Home extends Component {
   render() {
     return (
       <>
-        <Header />
         <News />
       </>
     );
