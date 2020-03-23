b82870cf5a19ad53e7a28f4a4a4942df0a8aead1
collins njau <collinsnjau39@gmail.com>
2020-03-23 18:33:30 +0300

    Add navigation back to the home page

--- src/components/savedNews/sideBar.js
+++ src/components/savedNews/sideBar.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const SideBar = () => {
   return (
     <div>
-      <div className="side_bar_element">Home</div>
+      <div className="side_bar_element">
+        <Link to="/">
+          {" "}
+          <span>Home</span>
+        </Link>
+      </div>
       <div className="side_bar_element">Profile</div>
       <div className="side_bar_element">Trending</div>
     </div>
