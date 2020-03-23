afee550846a063b68bbe8291198c634749e3f7dd
collins njau <collinsnjau39@gmail.com>
2020-03-23 18:29:53 +0300

    Add dashboard side navigation

--- src/components/savedNews/sideBar.js
+++ src/components/savedNews/sideBar.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const SideBar = () => {
-  return <div>Navigator</div>;
+  return (
+    <div>
+      <div className="side_bar_element">Home</div>
+      <div className="side_bar_element">Profile</div>
+      <div className="side_bar_element">Trending</div>
+    </div>
+  );
 };
