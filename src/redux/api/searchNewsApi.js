ecec57700b1a2545741153a90f06202f12208bf2
collins njau <collinsnjau39@gmail.com>
2020-08-17 15:38:53 +0300

    Fix search for news articles

--- src/redux/api/searchNewsApi.js
+++ src/redux/api/searchNewsApi.js
@@ -4,8 +4,9 @@ require("dotenv").config();
 class SearchNewsApi {
   static searchNewsArticles(query) {
     const newsApiKey = process.env.REACT_APP_NEWS_API_KEY;
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
     return axios.get(
-      `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${query}&apiKey=${newsApiKey}`
+      `${proxyUrl}https://gnews.io/api/v3/search?q=${query}&token=${newsApiKey}`
     );
   }
 }
